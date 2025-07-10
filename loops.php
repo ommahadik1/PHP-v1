12b3252f57d1e832b07c6e993d5283b9f76ed9cd
Om Mahadik <ommahadik245@gmail.com>
2025-07-10 21:23:34 +0530

    loops in php

---
+++ loops.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // Loops in php
+    echo "Loops in PHP<br>";
+    // For loop example
+    $i = 0;
+    for ($i = 0; $i < 5; $i++) {
+        echo "This is iteration number $i<br>";
+    }
+    echo "<br>";
+    // While loop example
+    $j = 0;
+    while ($j < 5) {
+        echo "This is iteration number $j<br>";
+        $j++;
+    }
+    echo "<br>";
+    // Do-while loop example
+    $k = 0;
+    do {
+        echo "This is iteration number $k<br>";
+        $k++;
+
+    } while ($k < 5);
+    echo "<br>";
+    ?>
+
+
+</body>
+
+</html>
